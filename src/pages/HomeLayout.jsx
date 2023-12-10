a4c9d741a361fb43fdf2003c09d4e507f662a846
MrYogesh0709 <yogeshvanzara98@gmail.com>
2023-12-11 02:19:36 +0530

    github link added

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,10 +1,17 @@
 import Navbar from "../Navbar";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import github from "../assets/github.svg";
 const HomeLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Link
+        to="https://github.com/MrYogesh0709/responsiveNavbar"
+        target="_blank"
+      >
+        <img src={github} alt="github" className="github" />
+      </Link>
     </>
   );
 };
